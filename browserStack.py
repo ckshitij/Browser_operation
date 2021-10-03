c9df8af705e3ef013cd99a1a1e3322f8ada90759
Kshitij Chaurasiya <kshitij.chaurasiya@gmail.com>
2021-10-03 14:30:25 +0530

    refactor code

--- browserStack.py
+++ browserStack.py
@@ -9,17 +9,17 @@ def is_supported_browser(browser_type):
         return True
     return False
 
-def get_browser_path(browser_type):
+def get_browser(browser_type):
     if(browser_type == 'chrome'):
-        return "google-chrome"
+        return "/usr/bin/google-chrome"
     elif(browser_type == 'firefox'):
-        return "firefox"
+        return "/usr/bin/firefox"
 
-def get_cache_path(browser_type):
+def get_cache_path(browser_type, username):
     if(browser_type == 'chrome'):
-        return "/home/ckshitij/.config/google-chrome"
+        return "/home/"+ username +"/.config/google-chrome"
     elif(browser_type == 'firefox'):
-        return "/home/ckshitij/.mozilla/firefox"
+        return "/home/"+ username +"/.mozilla/firefox"
         
 
 @app.route("/start", methods = ['GET'])
@@ -34,7 +34,7 @@ def get_browser_instance():
     if(is_supported_browser(browser_type) == False):
         return "Not Supported! only firefox and chrome are supported", 403
 
-    cmd_open = get_browser_path(browser_type) + " " + url
+    cmd_open = get_browser(browser_type) + " " + url
     os.system(cmd_open)
         
     return f'{browser_type} Browser instanse is opened', 200
@@ -62,24 +62,10 @@ def clear_browser():
     if(is_supported_browser(browser_type) == False):
         return "Not Supported! only firefox and chrome are supported", 403
 
-    remove_cmd = "rm -rfv " + get_cache_path(browser_type)
+    uname = "user name"
+    remove_cmd = "rm -rfv " + get_cache_path(browser_type, uname)
     os.system(remove_cmd); 
     return f'{browser_type} Browser chached information is cleared', 200
-
-
-# @app.route("/geturl", methods = ['GET'])
-# def clear_browser():
-#     browser_type = request.args.get('browser')
-
-#     if(browser_type == None):
-#         return "Bad Request! Browser information is missing", 400 
-
-#     if(is_supported_browser(browser_type) == False):
-#         return "Not Supported! only firefox and chrome are supported", 403
-
-#     remove_cmd = "rm -rfv " + get_cache_path(browser_type)
-#     os.system(remove_cmd); 
-#     return f'{browser_type} Browser chached information is cleared', 200
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=6123)
